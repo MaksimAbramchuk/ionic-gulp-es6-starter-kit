6e8a0f58bca90d006f999d2e54dc916f5bb1b203
Diego Vieira <diego@protos.inf.br>
2016-03-30 00:39:50 -0300

    del no longer requires a callback

--- gulp/build.js
+++ gulp/build.js
@@ -86,8 +86,8 @@ gulp.task('other', function () {
     .pipe(gulp.dest(path.join(conf.paths.dist, '/')));
 });
 
-gulp.task('clean', function (done) {
-  $.del([path.join(conf.paths.dist, '/'), path.join(conf.paths.tmp, '/')], done);
+gulp.task('clean', function () {
+  return $.del([path.join(conf.paths.dist, '/'), path.join(conf.paths.tmp, '/')]);
 });
 
 gulp.task('build', ['html', 'fonts', 'other']);
